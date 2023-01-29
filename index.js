33bc4d95ad1ad3583af701ba063b011fe8e08bf4
rahul007-bit <yadavrahul8141@yahoo.com>
2023-01-29 17:37:31 +0530

    add dockerfile

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.options("*", cors());
 
 mongoose.set("strictQuery", false);
 mongoose.connect(
-  "mongodb://localhost:27017/storage-system",
+  process.env.MONGO_URL || "mongodb://localhost:27017/storage-system",
   {
     useNewUrlParser: true,
   },
